a29e12a743900ad5901d95962d32865e0ae79bfa
MaiNguyenITC <nymar.v@itcgroup.io>
2025-04-28 14:05:56 +0700

    add filter to decode jwt

--- src/main/java/com/example/movie_service/filter/CustomSecurityContextFilter.java
+++ src/main/java/com/example/movie_service/filter/CustomSecurityContextFilter.java
@@ -1,18 +1,28 @@
 package com.example.movie_service.filter;
 
+import com.example.movie_service.constant.JwtConstant;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 @Component
 public class CustomSecurityContextFilter extends OncePerRequestFilter {
@@ -20,22 +30,25 @@ public class CustomSecurityContextFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String username = request.getHeader("X-User-Name");
-        String roles = request.getHeader("X-User-Roles");
+        String jwt = request.getHeader(JwtConstant.JWT_HEADER);
 
-        if (username != null && roles != null) {
+        System.out.println(jwt);
 
-            SimpleGrantedAuthority authority = new SimpleGrantedAuthority(roles);
+        if (jwt != null) {
+            jwt = jwt.substring(7);
+            try {
+                SecretKey secretKey = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+                Claims claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(jwt).getBody();
 
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    username, null, Collections.singletonList(authority)
-            );
+                String userName = String.valueOf(claims.get("username"));
+                String authorities = String.valueOf(claims.get("authorities"));
 
-            SecurityContext context = SecurityContextHolder.createEmptyContext();
-            context.setAuthentication(authentication);
-
-
-            SecurityContextHolder.setContext(context);
+                List<GrantedAuthority> auth = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(userName, null, auth);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (Exception e) {
+                throw new BadCredentialsException("invalid token.....");
+            }
         }
 
         filterChain.doFilter(request, response);
